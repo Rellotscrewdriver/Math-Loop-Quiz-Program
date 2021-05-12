55f39dbf00cc4d482ed592fcdb35973a4335a713
Rellotscrewdriver <JKanimations71@gmail.com>
2021-05-12 13:46:56 +0530

    added working multiple options

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 //asks the for loop of main function to print out number of questions to be attempted
 int number_Of_Questions;
-int correct_answer;
+float correct_answer;
 int your_attempt;
 //Counts How many answers were either correct/wrong in the for loop in main function
 int wrong_answer_count = 0;
 int right_answer_count = 0;
 /*
+The array numbers mean
    * 1 = Addition
    * 2 = Subtraction
    * 3 = Multiplication
@@ -20,6 +21,13 @@ int operators[] = {1, 2, 3, 4};
 //these two stores the random numbers generated and perform the operations in different functions
 int random_number1;
 int random_number2;
+int option[1]; //it can only choose between two, but in the future, I will add it upto 4
+/*If the option variable matches the correct answer, it will generate a new variable.
+  The chances of this occuring is pretty low, but still it should do the trick. */
+int new_random;
+//this
+int place_option;
+
 
 /*
     Generate Random Questions And Handle Their Answers For Addition, Subtraction, Multiplication
@@ -38,16 +46,21 @@ int main(){
 	cin >> number_Of_Questions;
 	cout << endl;
 
-	/* Now this bad boy can do addition, subtraction, multiplication and division
-       but the correct answers are always on right side
+	/* the correct answers are now dynamic. but the options are only 2.
+       In the future, I will add upto 4 and probably work on input function
+       not to excute when a string is entered
 	*/
 	for(int a = 1; a <= number_Of_Questions; a++){
 
     random_number1 = rand() % 10;
     random_number2 = rand() % 10;
+    new_random = rand() % 20;
+    option[0] = rand() % 20;
+    option[1] = rand() % 20;
+    place_option = rand() % 2;
 
     cout << "Question: " << a << endl;
-    if(operators[rand() % 4] == operators[0]){
+    if(operators[rand() % 4] == operators[0])
        GRQATAFA();
     else if(operators[rand() % 4] == operators[1])
        GRQATAFS();
@@ -65,12 +78,31 @@ int main(){
     return 0;
 }
 
+
+void option_choose(){
+    if(place_option == 1)
+    cout << "Options: " << new_random << " or " << correct_answer << "\nYour Answer: ";
+    else
+    cout << "Options: " << correct_answer << " or " << new_random << "\nYour Answer: ";
+
+}
+
+void options(){
+	if(option[0] == option[1])
+        option_choose();
+	else if(option[0] == correct_answer || option[1] == correct_answer)
+        option_choose();
+    else
+        option_choose();
+}
+
 //these functions generate random question and handle the answers
 //I had to create four fricking functions for the program to ask four different questions!
+
 void GRQATAFA(){
     correct_answer = random_number1 + random_number2;
 	cout << random_number1 << " + " << random_number2 << " = " << " ?\n";
-	cout << "Options: " << rand() % 10 << " or " << correct_answer << "\nYour Answer: " << endl;
+    options();
 	cin >> your_attempt;
 
 	if(your_attempt == (correct_answer)){
@@ -83,9 +115,9 @@ void GRQATAFA(){
 }
 
 void GRQATAFS(){
-    correct_answer = random_number1 + random_number2;
-	cout << random_number1 << " + " << random_number2 << " = " << " ?\n";
-	cout << "Options: " << rand() % 10 << " or " << correct_answer << "\nYour Answer: " << endl;
+    correct_answer = random_number1 - random_number2;
+	cout << random_number1 << " - " << random_number2 << " = " << " ?\n";
+    options();
 	cin >> your_attempt;
 
 	if(your_attempt == (correct_answer)){
@@ -100,7 +132,7 @@ void GRQATAFS(){
 void GRQATAFM(){
     correct_answer = random_number1 * random_number2;
 	cout << random_number1 << " X " << random_number2 << " = " << " ?\n";
-	cout << "Options: " << rand() % 10 << " or " << correct_answer << "\nYour Answer: " << endl;
+    options();
 	cin >> your_attempt;
 
 	if(your_attempt == (correct_answer)){
@@ -115,7 +147,7 @@ void GRQATAFM(){
 void GRQATAFD(){
     correct_answer = random_number1 / random_number2;
 	cout << random_number1 << " / " << random_number2 << " = " << " ?\n";
-	cout << "Options: " << rand() % 10 << " or " << correct_answer << "\nYour Answer: " << endl;
+    options();
 	cin >> your_attempt;
 
 	if(your_attempt == (correct_answer)){
