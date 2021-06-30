1288341d6d75eb4cd2c04fab426c3df54857d469
Rellotscrewdriver <JKanimations71@gmail.com>
2021-06-30 14:03:53 +0530

    refactored some code, precise division operation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
+
 using namespace std;
 
 //asks the for loop of main function to print out number of questions to be attempted
 int number_Of_Questions;
-int correct_answer;
-int your_attempt;
+float correct_answer;
+float your_attempt;
+
 //Counts How many answers were either correct/wrong in the for loop in main function
 int wrong_answer_count = 0;
 int right_answer_count = 0;
@@ -17,29 +20,24 @@ The array numbers mean
    * 3 = Multiplication
    * 4 = Division
 */
-int operators[] = {1, 2, 3, 4};
+char operators[4] = {'+', '-', '*', '/'};
 //these two stores the random numbers generated and perform the operations in different functions
-int random_number1;
-int random_number2;
-int option[1]; //it can only choose between two, but in the future, I will add it upto 4
+float random_number1;
+float random_number2;
+int option[1]; //it can only choose between two
 /*If the option variable matches the correct answer, it will generate a new variable.
   The chances of this occuring is pretty low, but still it should do the trick. */
-int new_random;
+int new_random_number;
+int option_order;
 
-int place_option;
 
 
-/*
-    Generate Random Questions And Handle Their Answers For Addition, Subtraction, Multiplication
-    and Division
-*/
-void GRQATAFA();
-void GRQATAFS();
-void GRQATAFM();
-void GRQATAFD();
+void generate_addtion_question();
+void generate_subtract_question();
+void generate_multiply_question();
+void generate_division_question();
 
 int main(){
-
 	srand(time(0));
 
 	cout << "How many questions?" << "\nAnswer: ";
@@ -53,21 +51,21 @@ int main(){
 
     random_number1 = 2 + (rand() % 10);
     random_number2 = 2 + (rand() % 10);
-    new_random = 2.0 + (rand() % 20);
+    new_random_number = -69.0 + (rand() % 2);
     option[0] = 2 + (rand() % 20);
     option[1] = 2 + (rand() % 20);
-    place_option = rand() % 2;
+    option_order = rand() % 2;
 
 
     cout << "Question: " << a << endl;
     if(operators[rand() % 4] == operators[0])
-       GRQATAFA();
+       generate_division_question();
     else if(operators[rand() % 4] == operators[1])
-       GRQATAFS();
+       generate_division_question();
     else if(operators[rand() % 4] == operators[2])
-       GRQATAFM();
+       generate_division_question();
     else
-       GRQATAFD();
+       generate_division_question();
 
         cout << endl;
     }
@@ -79,94 +77,70 @@ int main(){
     return 0;
 }
 
-
-void option_choose(){
-    if(place_option == 1)
-    cout << "Options: " << new_random << " or " << correct_answer << "\nYour Answer: ";
+void choose_option(){
+    if(option_order == 1)
+    cout << "Options: " << new_random_number << " or " << correct_answer << "\nYour Answer: ";
     else
-    cout << "Options: " << correct_answer << " or " << new_random << "\nYour Answer: ";
+    cout << "Options: " << correct_answer << " or " << new_random_number << "\nYour Answer: ";
 
 }
 
-void options(){
+void show_options(){
 	if(option[0] == option[1])
-        option_choose();
+        choose_option();
 	else if(option[0] == correct_answer && option[1] == correct_answer)
-        option_choose();
+        choose_option();
     else
-        option_choose();
+        choose_option();
 }
 
-void fdgd(){
-//try{
-	if(your_attempt == (correct_answer)){
+void check_answer(){
+
+	if(your_attempt == correct_answer){
 		cout << "Congradulations, that is correct" << endl;
 		right_answer_count += 1;
-	} else if(your_attempt == new_random || your_attempt != your_attempt){
+	} else if(your_attempt == new_random_number || your_attempt != your_attempt){
 		cout << "You are wrong, the answer was " << correct_answer << endl;
 		wrong_answer_count += 1;
 	} else {
-       // throw "Invaild Number: ";
+	    cin.clear();
+	    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "You are wrong, the answer was " << correct_answer << endl;
+		wrong_answer_count += 1;
 	}
-//}
-	//catch(const char* l){
-     //   cout << "you attempted to write a text or a letter instead of integer" << endl;
-    //    std::terminate();
-	//}
+
 }
 //these functions generate random question and handle the answers
 //I had to create four fricking functions for the program to ask four different questions!
 
-void GRQATAFA(){
+void generate_addtion_question(){
     correct_answer = random_number1 + random_number2;
 	cout << random_number1 << " + " << random_number2 << " = " << " ?\n";
-    options();
+    show_options();
 	cin >> your_attempt;
-
-    fdgd();
+    check_answer();
 }
 
-void GRQATAFS(){
+void generate_subtract_question(){
     correct_answer = random_number1 - random_number2;
 	cout << random_number1 << " - " << random_number2 << " = " << " ?\n";
-    options();
+    show_options();
 	cin >> your_attempt;
-
-	if(your_attempt == (correct_answer)){
-		cout << "Congratulations, that is correct" << endl;
-		right_answer_count += 1;
-	} else {
-		cout << "You are wrong, the answer was " << correct_answer << endl;
-		wrong_answer_count += 1;
-	}
+    check_answer();
 }
 
-void GRQATAFM(){
+void generate_multiply_question(){
     correct_answer = random_number1 * random_number2;
 	cout << random_number1 << " X " << random_number2 << " = " << " ?\n";
-    options();
+    show_options();
 	cin >> your_attempt;
-
-	if(your_attempt == (correct_answer)){
-		cout << "Congratulations, that is correct" << endl;
-		right_answer_count += 1;
-	} else {
-		cout << "You are wrong, the answer was " << correct_answer << endl;
-		wrong_answer_count += 1;
-	}
+    check_answer();
 }
 
-void GRQATAFD(){
+void generate_division_question(){
     correct_answer = random_number1 / random_number2;
 	cout << random_number1 << " / " << random_number2 << " = " << " ?\n";
-    options();
+    show_options();
 	cin >> your_attempt;
-
-	if(your_attempt == (correct_answer)){
-		cout << "Congratulations, that is correct" << endl;
-		right_answer_count += 1;
-	} else {
-		cout << "You are wrong, the answer was " << correct_answer << endl;
-		wrong_answer_count += 1;
-    }
+    check_answer();
 }
