45d446e12fc2857291e2bba5b35e04bde1aef274
Rellotscrewdriver <JKanimations71@gmail.com>
2021-07-29 11:17:26 +0530

    putted all the functions into classes

--- main.cpp
+++ main.cpp
@@ -1,146 +1,242 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <limits>
 
 using namespace std;
 
-//asks the for loop of main function to print out number of questions to be attempted
-int number_Of_Questions;
-float correct_answer;
-float your_attempt;
-
+char operators[4] = {'+', '-', '*', '/'};
 //Counts How many answers were either correct/wrong in the for loop in main function
 int wrong_answer_count = 0;
 int right_answer_count = 0;
-/*
-The array numbers mean
-   * 1 = Addition
-   * 2 = Subtraction
-   * 3 = Multiplication
-   * 4 = Division
-*/
-char operators[4] = {'+', '-', '*', '/'};
-//these two stores the random numbers generated and perform the operations in different functions
-float random_number1;
-float random_number2;
-int option[1]; //it can only choose between two
-/*If the option variable matches the correct answer, it will generate a new variable.
-  The chances of this occuring is pretty low, but still it should do the trick. */
-int new_random_number;
-int option_order;
 
 
+struct variables {
+	//asks the for loop of main function to print out number of questions to be attempted
+	int number_Of_Questions;
+	//these two data types were changed in order to be able
+	float correct_answer;
+	float your_attempt;
 
-void generate_addtion_question();
-void generate_subtract_question();
-void generate_multiply_question();
-void generate_division_question();
+	//these two stores the random numbers generated and perform the operations in different functions
+	float random_number1;
+	float random_number2;
+	int option[1]; //it can only choose between two
+	/*If the option variable matches the correct answer, it will generate a new variable.
+	  The chances of this occurring is pretty low, but still it should do the trick. */
+	int new_random_number;
+	int option_order;
+} var ;
 
-int main(){
-	srand(time(0));
-
-	cout << "How many questions?" << "\nAnswer: ";
-	cin >> number_Of_Questions;
-	cout << endl;
-
-	/* edit: I rather scrap that last idea and call it done. I would focus more on
-        refactoring, cleaning and tiding code and probably fix some bugs
-	*/
-	for(int a = 1; a <= number_Of_Questions; a++){
-
-    random_number1 = 2 + (rand() % 10);
-    random_number2 = 2 + (rand() % 10);
-    new_random_number = -69.0 + (rand() % 2);
-    option[0] = 2 + (rand() % 20);
-    option[1] = 2 + (rand() % 20);
-    option_order = rand() % 2;
-
-
-    cout << "Question: " << a << endl;
-    if(operators[rand() % 4] == operators[0])
-       generate_division_question();
-    else if(operators[rand() % 4] == operators[1])
-       generate_division_question();
-    else if(operators[rand() % 4] == operators[2])
-       generate_division_question();
-    else
-       generate_division_question();
-
-        cout << endl;
-    }
-
-	cout << "\n\nNumber of Correct answers: " << right_answer_count << endl;
-	cout << "Number of Wrong answers: " << wrong_answer_count << endl;
-
-	system("pause");
-    return 0;
-}
-
-void choose_option(){
-    if(option_order == 1)
-    cout << "Options: " << new_random_number << " or " << correct_answer << "\nYour Answer: ";
-    else
-    cout << "Options: " << correct_answer << " or " << new_random_number << "\nYour Answer: ";
-
-}
-
-void show_options(){
-	if(option[0] == option[1])
-        choose_option();
-	else if(option[0] == correct_answer && option[1] == correct_answer)
-        choose_option();
-    else
-        choose_option();
-}
-
-void check_answer(){
-
-	if(your_attempt == correct_answer){
-		cout << "Congradulations, that is correct" << endl;
-		right_answer_count += 1;
-	} else if(your_attempt == new_random_number || your_attempt != your_attempt){
-		cout << "You are wrong, the answer was " << correct_answer << endl;
-		wrong_answer_count += 1;
-	} else {
-	    cin.clear();
-	    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "You are wrong, the answer was " << correct_answer << endl;
-		wrong_answer_count += 1;
+class option {
+private:
+	void choose_new_option() {
+		if (var.option_order == 1)
+			cout << "Options: " << var.new_random_number << " or " << var.correct_answer << "\nYour Answer: ";
+		else
+			cout << "Options: " << var.correct_answer << " or " << var.new_random_number << "\nYour Answer: ";
 	}
 
-}
-//these functions generate random question and handle the answers
-//I had to create four fricking functions for the program to ask four different questions!
+    void choose_option() {
+		if (var.option_order == 1)
+			cout << "Options: " << var.option[0] << " or " << var.correct_answer << "\nYour Answer: ";
+		else
+			cout << "Options: " << var.correct_answer << " or " << var.option[1] << "\nYour Answer: ";
+	}
 
-void generate_addtion_question(){
-    correct_answer = random_number1 + random_number2;
-	cout << random_number1 << " + " << random_number2 << " = " << " ?\n";
-    show_options();
-	cin >> your_attempt;
-    check_answer();
+public:
+	void show_options() {
+		if (var.option[0] == var.option[1]){
+			choose_new_option();
+		} else if (var.option[0] == var.correct_answer && var.option[1] == var.correct_answer){
+			choose_new_option();
+        } else {
+            choose_option();
+		}
+	}
+
+};
+
+
+class Solve {
+public:
+	void check_answer() {
+		if (var.your_attempt == var.correct_answer) {
+			cout << "Congradulations, that is correct" << endl;
+			right_answer_count++;
+		} else {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "You are wrong, the answer was " << var.correct_answer << endl;
+			wrong_answer_count++;
+		}
+	}
+};
+
+
+class Generate {
+
+private:
+
+	void question_format(string op) { //op means operator
+		option o;
+		Solve s;
+		cout << var.random_number1 << op << var.random_number2 << " = " << " ?\n";
+		o.show_options();
+		cin >> var.your_attempt;
+		s.check_answer();
+	}
+
+public:
+
+	float generate_addition_question(int random_number1, int random_number2) {
+		var.correct_answer = random_number1 + random_number2;
+		question_format(" + ");
+		return var.correct_answer;
+	}
+
+	float generate_subtract_question(int random_number1, int random_number2) {
+		var.correct_answer = random_number1 - random_number2;
+		question_format(" - ");
+		return var.correct_answer;
+	}
+
+	float generate_multiply_question(int random_number1, int random_number2) {
+		var.correct_answer = random_number1 * random_number2;
+		question_format(" * ");
+		return var.correct_answer;
+	}
+
+	float generate_division_question(int random_number1, int random_number2) {
+		var.correct_answer = random_number1 / random_number2;
+		question_format(" / ");
+        return var.correct_answer;
+	}
+
+};
+
+class Display {
+
+public:
+	void numberOfTimes(int number_Of_Questions) {
+		Generate gen;
+
+		for (int a = 1; a <= number_Of_Questions; a++) {
+			var.random_number1 = 2.0 + fmod(rand(), 10.0);
+			var.random_number2 = 2.0 + fmod(rand(), 10.0);
+			var.new_random_number = -69.0 + fmod(rand(), 1.0);
+			var.option[0] = 2 + fmod(rand(), 10.0);
+			var.option[1] = 2 + fmod(rand(), 10.0);
+			var.option_order = rand() % 2;
+
+			cout << "Question: " << a << endl;
+			if (operators[rand() % 4] == operators[0])
+				gen.generate_addition_question(var.random_number1, var.random_number2);
+			else if (operators[rand() % 4] == operators[1])
+				gen.generate_subtract_question(var.random_number1, var.random_number2);
+			else if (operators[rand() % 4] == operators[2])
+				gen.generate_multiply_question(var.random_number1, var.random_number2);
+			else
+				gen.generate_division_question(var.random_number1, var.random_number2);
+			cout << endl;
+		}
+	}
+
+	//display random question
+	//show option
+};
+
+/*
+    Class Generate{
+        question format
+        generate questions
+    }
+
+    Class Solve {
+        check answer
+    }
+
+    Class Options {
+        choose option
+    }
+    Class Display{
+        show number of times
+        display random question
+    }
+*/
+int main() {
+	srand(time(0));
+	cout << "How many questions?" << "\nAnswer: ";
+	cin >> var.number_Of_Questions;
+	Display show;
+	show.numberOfTimes(var.number_Of_Questions);
+
+	cout << "\n\nNumber of Correct answers: " << right_answer_count << endl;
+	cout << "Number of InCorrect answers: " << wrong_answer_count << endl;
+	system("pause");
+	return 0;
 }
 
-void generate_subtract_question(){
-    correct_answer = random_number1 - random_number2;
-	cout << random_number1 << " - " << random_number2 << " = " << " ?\n";
-    show_options();
-	cin >> your_attempt;
-    check_answer();
-}
+/*
+#include <iostream>
+#include<cstdlib>
+using namespace std;
 
-void generate_multiply_question(){
-    correct_answer = random_number1 * random_number2;
-	cout << random_number1 << " X " << random_number2 << " = " << " ?\n";
-    show_options();
-	cin >> your_attempt;
-    check_answer();
-}
+class Question {
+    public:
+        int correctAnswerCount = 0;
+        int wrongAnswerCount = 0;
+        Question(){
+            srand(time(0));
+        }
 
-void generate_division_question(){
-    correct_answer = random_number1 / random_number2;
-	cout << random_number1 << " / " << random_number2 << " = " << " ?\n";
-    show_options();
-	cin >> your_attempt;
-    check_answer();
+        string signCalc[4] = {"-" , "+" , "/", "*"};
+        int generateRandNumber () {
+            return (rand() % 50) +10;
+        }
+
+        int generateQuestions () {
+
+            string sign = signCalc[rand() % 4];
+
+            int secondNumber = generateRandNumber();
+            int firstNumber = generateRandNumber();
+
+            cout << to_string(firstNumber) + " " + sign + " " + to_string(secondNumber) + " ?"<< endl;
+
+            int correctAnswer = (sign == "+") ? firstNumber + secondNumber:
+                                (sign == "/") ? firstNumber / secondNumber:
+                                (sign == "-") ? firstNumber - secondNumber:
+                                firstNumber * secondNumber;
+
+            return correctAnswer;
+        }
+
+        int noOfTimes(int y){
+            for(int i = 0; i < y; i++){
+                cout << "question" + to_string(i) << endl;
+                int corretAnswer = generateQuestions();
+                cout << "ans ? " << endl;
+                int userChoice;
+                cin >> userChoice;
+
+                if(userChoice == corretAnswer){
+                    correctAnswerCount++;
+                }else{
+                    wrongAnswerCount++;
+                }
+            }
+            cout << "Ans correct " << correctAnswerCount << endl;
+            cout << "Ans wrong " << wrongAnswerCount << endl;
+            return 0;
+        }
+
+};
+
+int main(){
+    Question firstq;
+    //take user input and pass it to noOfTimes
+    firstq.noOfTimes(2);
 }
+*/
